d8ab31ca38ee4a74fd142511a762621b6004c603
Eduardo Ramos <luis.ramos@metasix.com.br>
2020-02-04 13:57:10 -0300

    Tipagem generica

--- app/js/views/View.js
+++ app/js/views/View.js
@@ -1,5 +1,12 @@
+// <T> é uma convenção para dizer que o tipo de algo é generico
 class View {
     constructor(selector) {
         this._element = document.querySelector(selector);
+    }
+    update(model) {
+        this._element.innerHTML = this.template(model);
+    }
+    template(model) {
+        throw new Error("Metodo template não definido");
     }
 }
--- app/ts/views/View.ts
+++ app/ts/views/View.ts
@@ -1,7 +1,16 @@
-class View {
+// <T> é uma convenção para dizer que o tipo de algo é generico
+class View<T> {
   protected _element: Element;
 
   constructor(selector: string) {
     this._element = document.querySelector(selector);
+  }
+
+  update(model: T) {
+    this._element.innerHTML = this.template(model);
+  }
+
+  template(model: T): string {
+    throw new Error("Metodo template não definido");
   }
 }
